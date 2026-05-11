4ce2d007fcb0d4ba6d67bd95bab83e84d9e453c6
Anthony labrosse <labrosse.anthony.dev@gmail.com>
2026-05-11 14:00:36 +0200

    Création du composant Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import './main.css';
 
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<div>Page de connexion en construction</div>} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
