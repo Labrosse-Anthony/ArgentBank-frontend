01ca45da363bec93f63e2f31b7e5e845905dde1c
Anthony labrosse <labrosse.anthony.dev@gmail.com>
2026-05-12 11:12:46 +0200

    Intégration de la page login

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import './index.css';
 import './main.css';
+import Login from './pages/login';
 
 function App() {
   return (
@@ -12,7 +14,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<div>Page de connexion en construction</div>} />
+          <Route path="/login" element={<Login />} />
         </Routes>
         <Footer />
       </div>
