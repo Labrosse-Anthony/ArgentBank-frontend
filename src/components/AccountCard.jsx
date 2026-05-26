7501d2fda58d0e78721464097cb4ca367a28d085
Anthony labrosse <labrosse.anthony.dev@gmail.com>
2026-05-26 10:16:47 +0200

    Ajout param dans AccoundCard.jsx

--- src/components/AccountCard.jsx
+++ src/components/AccountCard.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+/** * Composant pour afficher les informations d'un compte bancaire
+ * @param {string} title - Le titre du compte (ex: "Argent Bank Checking")
+ * @param {string} amount - Le montant actuel du compte (ex: "$2,082.79")
+ * @param {string} description - Une description du compte (ex: "Available Balance")
+ */
 
 function AccountCard({ title, amount, description }) {
   return (
